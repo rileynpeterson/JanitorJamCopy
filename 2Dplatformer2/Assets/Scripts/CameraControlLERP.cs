65c08712cdaedb15bdabf752343740678fc91c56
rileynpeterson <rpeterson114@gmail.com>
2021-03-15 17:32:43 -0400

    LERP for camera

---
+++ 2Dplatformer2/Assets/Scripts/CameraControlLERP.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControlLERP : MonoBehaviour
+{
+    public GameObject target;
+
+    void FixedUpdate()
+    {
+        Vector2 pos = Vector2.Lerp((Vector2)transform.position, (Vector2)target.transform.position, Time.fixedDeltaTime);
+        //transform.position = new Vector3 (pos.x, pos.y, transform.position.y);
+        transform.position = new Vector3(pos.x, pos.y, transform.position.z);
+    }
+}
+
